c17abd1e9aca2ab489902ba3fcc21d24bcc86709
AlexTs <alextsishuk@gmail.com>
2020-03-04 17:24:57 +0300

    Add food paint, increase length number when eat. Only snake head without body

--- snake.c
+++ snake.c
@@ -12,14 +12,25 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define XMAX 20
 #define YMAX 40
 
 int direction;
-int xpos = 10;
-int ypos = 20;
+//int snake_x_pos = 10;
+//int snake_y_pos = 20;
+int snake_length = 1;
 int my_mutex = 0;
+int counter = 0;
+int fruit_x_pos=2, fruit_y_pos=2;
+
+struct Point{
+	int X;
+	int Y;
+};
+
+struct Point snake = {20,10};
 
 int mygetch( ) {
 	struct termios oldt,
@@ -38,28 +49,69 @@ void clrscr(void);
 void print_grid(int X_MAX, int Y_MAX);
 
 
+void printSnakeInfo(void){
+	printf("\033[%d;%dH       ",21,42);	// Clear previous X:
+	printf("\033[%d;%dH",21,42);		// Set cursor on the current position
+	printf("X:%d",snake.X);				// Print X:
+	printf("\033[%d;%dH       ",22,42);	// Clear previous Y:
+	printf("\033[%d;%dH",22,42);		
+	printf("Y:%d",snake.Y);
+	printf("\033[%d;%dH           ",23,42);	// Clear previous length:
+	printf("\033[%d;%dH",23,42);		
+	printf("Length:%d",snake_length);
+}
+
+
+// void printFruitXY(void){
+// 	printf("\033[%d;%dH       ",15,42);	// Clear previous X:
+// 	printf("\033[%d;%dH",15,42);		// Set cursor on the current position
+// 	printf("FrX:%d",fruit_x_pos);				// Print X:
+// 	printf("\033[%d;%dH       ",16,42);	// Clear previous Y:
+// 	printf("\033[%d;%dH",16,42);		
+// 	printf("FrY:%d",fruit_y_pos);
+// }
+
+void generateFruit(void){
+	fruit_x_pos = 2+(rand()%(XMAX-1));
+	fruit_y_pos = 2+(rand()%(YMAX-1));
+	printf("\033[%d;%dHF",fruit_x_pos,fruit_y_pos);		// Paint new fruit
+}
+
+
 void* thread_func(void* arg){
 	while(1){
 		my_mutex = 0;
-		printf("\033[%d;%dH ",xpos,ypos);	// Clear current position symbol
+		printf("\033[%d;%dH ",snake.X,snake.Y);	// Clear current position symbol
 		switch(direction){
-			case 3:	ypos++;
+			case 3:	snake.Y++;
 					break;
-			case 6:	xpos++;
+			case 6:	snake.X++;
 					break;
-			case 9:	ypos--;
+			case 9:	snake.Y--;
 					break;
-			case 0:	xpos--;
+			case 0:	snake.X--;
 					break;
 		}
-		printf("\033[%d;%dH       ",21,42);	// Clear previous X:
-		printf("\033[%d;%dH",21,42);		// Set cursor on the current position
-		printf("X:%d",xpos);
-		printf("\033[%d;%dH       ",22,42);	// Clear previous Y:
-		printf("\033[%d;%dH",22,42);		
-		printf("Y:%d",ypos);
-		printf("\033[%d;%dH0",xpos,ypos);	// Print new "HEAD" of snake
-		fflush(stdout);		// force clear console buffer
+		if ((snake.X==fruit_x_pos)&&(snake.Y==fruit_y_pos)){
+			snake_length++;
+			generateFruit();
+		}
+		printSnakeInfo();
+
+		printf("\033[%d;%dH0",snake.X,snake.Y);	// Print new "HEAD" of snake
+
+
+		// counter++;
+		// if (counter == 5){
+		// 	counter = 0;
+		// 	printf("\033[%d;%dH ",fruit_x_pos,fruit_y_pos);		// Erase old fruit
+		// 	fruit_x_pos = 2+(rand()%(XMAX-1));
+		// 	fruit_y_pos = 2+(rand()%(YMAX-1));
+		// 	printf("\033[%d;%dHF",fruit_x_pos,fruit_y_pos);		// Paint new fruit
+		// 	//printFruitXY();
+		// }
+
+		fflush(stdout);						// force clear console buffer
 		my_mutex = 1;
 		usleep(500000);
 	}
@@ -72,7 +124,7 @@ int main()
 	pthread_cond_t* cond;
 	direction = 3;
 	int ch;
-
+	srand(time(0));
 	// allocate memory to cond (conditional variable),  
     // thread id's and array of size threads 
     cond = (pthread_cond_t*)malloc(sizeof(pthread_cond_t)); 
@@ -82,7 +134,10 @@ int main()
 	clrscr();
 	print_grid(XMAX,YMAX);
 	printf ("\e[?25h");	//set cursor VISIBLE
-	printf("\033[%d;%dH ",xpos,ypos);
+	fruit_x_pos = 2+(rand()%(XMAX-1));
+	fruit_y_pos = 2+(rand()%(YMAX-1));
+	printf("\033[%d;%dHF",fruit_x_pos,fruit_y_pos);		// Paint new fruit
+	printf("\033[%d;%dH ",snake.X,snake.Y);
 	sleep(1);
 	pthread_create(tid, NULL, thread_func, NULL);
 	
